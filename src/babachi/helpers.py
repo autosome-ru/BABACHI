56d8d1483c061078a8c11f4ac283d2f85e21714f
Sergey Abramov <aswq22013@gmail.com>
2022-04-30 17:59:32 +0300

    fix typo

--- src/babachi/helpers.py
+++ src/babachi/helpers.py
@@ -17,7 +17,7 @@ class ChromosomesWrapper:
             self.chromosomes = dict(zip(self.sorted_chromosomes, chr_l))
         else:
             self.chromosomes = pd.Series(chromosomes_df.length.values,
-                                         index=chromosomes_df.chromosomes)
+                                         index=chromosomes_df.chromosome)
             self.sorted_chromosomes = chromosomes_df.chromosomes.tolist()
 
 
